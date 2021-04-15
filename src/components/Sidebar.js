c6a0e70e3d1862cda4048a052ee7cc0bcab4e166
Alexandria <alex.pugia816@hotmail.com>
2021-04-15 11:58:35 -0400

    added styling to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,40 +1,55 @@
-import React from 'react'
-import { Segment, Menu, Card, Image } from 'semantic-ui-react'
+import React from "react";
+
+//styling
+import withStyles from "react-jss";
+import { Segment, Menu, Card, Image } from "semantic-ui-react";
 
 const Sidebar = (props) => {
-    const joinDate = props.activeUser.created_at.split('-')[0]
-    const totalMiles = props.activeUser.trips.reduce((total, trip) => total + trip.miles, 0)
+  const { classes, activeUser } = props;
+  const joinDate = activeUser.created_at.split("-")[0];
+  const totalMiles = activeUser.trips.reduce(
+    (total, trip) => total + trip.miles,
+    0
+  );
 
-    return (
-            <Menu vertical fluid>
-                <Card fluid>
-                    <Image 
-                        src={!props.activeUser.profile_pic 
-                            ? 'https://react.semantic-ui.com/images/avatar/large/molly.png' 
-                                : props.activeUser.profile_pic} 
-                        wrapped ui={false} />
-                    <Card.Content>
-                    <Card.Header>{props.activeUser.username}</Card.Header>
-                    <Card.Meta>
-                        <span className='date'>Joined in {joinDate}</span>
-                    </Card.Meta>
-                    <Card.Description>
-                        {props.activeUser.bio}
-                    </Card.Description>
-                    </Card.Content>
-                </Card>
+  return (
+    <Menu vertical fluid borderless className={classes.container}>
+      <Card fluid>
+        <Image
+          src={
+            activeUser.profile_pic ||
+            "https://react.semantic-ui.com/images/avatar/large/molly.png"
+          }
+          wrapped
+          ui={false}
+        />
+        <Card.Content>
+          <Card.Header>{activeUser.username}</Card.Header>
+          <Card.Meta>
+            <span className="date">Joined in {joinDate}</span>
+          </Card.Meta>
+          <Card.Description>{activeUser.bio}</Card.Description>
+        </Card.Content>
+      </Card>
 
-                <Segment>
-                    <Menu.Header>Total Stats</Menu.Header>
+      <Segment>
+        <Menu.Header>Total Stats</Menu.Header>
 
-                    <Menu.Menu>
-                        <Menu.Item>Trips: {props.activeUser.trips.length}</Menu.Item>
-                        <Menu.Item>Miles: {totalMiles ? totalMiles : 0 }</Menu.Item>
-                        <Menu.Item>Bags: {props.activeUser.luggages.length} </Menu.Item>
-                    </Menu.Menu>
-                </Segment>
-            </Menu>
-    )
-}
+        <Menu.Menu>
+          <Menu.Item>Trips: {activeUser.trips.length}</Menu.Item>
+          <Menu.Item>Miles: {totalMiles ? totalMiles : 0}</Menu.Item>
+          <Menu.Item>Bags: {activeUser.luggages.length} </Menu.Item>
+        </Menu.Menu>
+      </Segment>
+    </Menu>
+  );
+};
 
-export default Sidebar
+const styles = {
+  container: {
+    border: "none !important",
+    boxShadow: "0px 0px transparent !important",
+  },
+};
+
+export default withStyles(styles)(Sidebar);
